4e28d4c22bc79757fffdbf6ad5787ac866c3c2c8
ali <alistellar@yahoo.com>
2022-01-24 19:13:52 +0330

    Update to do list.js

--- asset/JS/to do list.js
+++ asset/JS/to do list.js
@@ -1,11 +1,13 @@
-let input = document.querySelector('.input') , 
-btn = document.querySelector('.btn'),
-chid2 = document.querySelector('.chid2');
+let input = document.querySelector('.input'),
+    btn = document.querySelector('.btn'),
+    chid2 = document.querySelector('.chid2');
 
-btn.addEventListener("click" , getValue);
+btn.addEventListener("click", getValue);
 
 
 
-function getValue(){
-    chid2.innerHTML = input.value
+function getValue(e) {
+    e.preventDefault()
+    chid2.innerHTML += input.value
+    input.value = ''
 }
